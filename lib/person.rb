fcabf6e9e118e9c8fd9e91a13b92f86ae2c88a5e
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-19 02:04:53 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,91 @@
-# your code goes here
+class Person
+  
+  attr_reader :name, :hygiene, :happiness
+  attr_accessor :bank_account 
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  #happiness will take in an added point and return the propper value
+  
+  def happiness=(points)
+    point_value = @happiness + points
+    if point_value > 10
+      @happiness = 10
+    elsif point_value < 0 
+      @happiness = 0 
+    else
+      @happiness = point_value
+    end
+  end
+  
+def hygiene=(points)
+    @hygiene = @hygiene + points
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    else
+      @hygiene = point_value
+    end
+  end
+
+  def clean?
+    if self.hygine > 7
+      true
+    else
+      false
+    end
+  end
+
+  def happy?
+    if self.happiness > 7
+      true
+    else
+      false
+    end
+  end
+
+  def get_paid(salary)
+    self.accounts += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygine=(4)
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+   happiness=(2)
+   hygine=(-3)
+   "♪ Rub-a-dub just relaxing in the tub ♫"
+
+  def call_friend(friend)
+    happiness=(3)
+    friend.happiness=(3)
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+ end
+
+
+   def start_conversation(person, topic)
+    if topic == "politics"
+      person.happiness=(-1)
+      happiness=(-1)
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      person.happiness=(1)
+      happiness=(1)
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+  
+end
+
